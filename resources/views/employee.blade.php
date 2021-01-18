7ed188e890d4fe6e08a9bb84371bb1ea665b5065
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-18 23:35:20 +0600

    search bar added

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -89,6 +89,15 @@
     </button>
 
     <br><br><br><br>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand">Data Table</a>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
     <table id="datatable" class="table table-dark table-striped">
         <thead>
             <th scope="col">Id</th>
